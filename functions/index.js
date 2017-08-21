840a5236f8c53fe584e09ff68e05806548da40ca
parnurzeal <parnurzeal@gmail.com>
2017-08-21 10:33:07 +0800

    1st version to get OMG price

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,31 @@
 const admin = require('firebase-admin');
 const auth = require('basic-auth');
+const axios = require('axios');
 const functions = require('firebase-functions');
-
-admin.initializeApp(functions.config().firebase);
 
 const config = functions.config();
 
+admin.initializeApp(config.firebase);
+
+class BxApi {
+  constructor() {}
+  getOmgToThb() {
+    const url = 'https://bx.in.th/api/';
+    return axios.get(url)
+      .then((res) => {
+        return res.data['26'];
+      });
+  }
+}
+
+const bxApi = new BxApi();
+
 exports.webhook = functions.https.onRequest((req, res) => {
   const credentials = auth(req);
+  if (!config.apiai) {
+    console.log('Config is not properly set');
+    return res.send(401);
+  }
   if (!credentials
     || credentials.name !== config.apiai.username
     || credentials.pass !== config.apiai.password) {
@@ -21,17 +39,14 @@ exports.webhook = functions.https.onRequest((req, res) => {
   const apiAiInput = req.body.result;
   const action = apiAiInput.action;
   const contexts = apiAiInput.contexts;
-  const text = 'from webhook';
-  return res.json({
-    speech: text,
-    displayText: text,
-    contextOut: contexts,
-    source: 'AtCoinWebhook',
+
+  bxApi.getOmgToThb().then((result) => {
+    const text = JSON.stringify(result, null, 2);
+    return res.json({
+      speech: text,
+      displayText: text,
+      contextOut: contexts,
+      source: 'AtCoinWebhook',
+    });
   });
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
